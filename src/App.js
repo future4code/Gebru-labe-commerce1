42d7ca4be2895032023984ff5acfec009e959bb1
milenyfaria <myrfaria@gmail.com>
2022-02-16 19:34:02 -0300

    desfiz alteracoes na master u.u

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header'
-import styled from 'styled-components';
+import styled from 'styled-components'
 import {Home} from './components/Home/Home'
 
 
